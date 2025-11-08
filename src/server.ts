eba8b5593de6d013a50f61d8a7b9d70199e76910
Ana <email_da_ana@exemplo.com>
2025-11-08 17:35:42 -0300

    agora sim

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,55 @@
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
-class App{
-    private app: express.Application
-    private http: http.Server
-    private io: Server
+import path from 'path'
 
-    constructor(){
-        this.app = express();
-        this.http = http.createServer(this.app);
-        this.io = new Server(this.http);
-        this.listenSockets();
-        this.setupRoutes();
-    }
-    listenServer(){
-        this.http.listen(3000, () => console.log('Server is running on port 3000'))
-    }
-    listenSockets(){
-        this.io.on('connection', (socket) => {
-            console.log('user connected:', socket.id)
+class App {
+  private app: express.Application
+  private http: http.Server
+  private io: Server
 
-            socket.on('message', (msg) => {
-                this.io.emit('message', msg)
-                console.log("file:server.ts:16 ~ App ~ socket.on msg:", msg)
-            })
-        })
-    }
-    setupRoutes(){
-        this.app.get('/', (req, res) => {
-            res.sendFile(__dirname + '/index.html');
+  constructor() {
+    this.app = express()
+    this.http = http.createServer(this.app)
+    this.io = new Server(this.http)
+    this.listenSockets()
+    this.setupRoutes()
+  }
+
+  listenServer() {
+    this.http.listen(3000, () => console.log('Server running on port 3000'))
+  }
+
+  listenSockets(){
+    this.io.on('connection', (socket) => {
+        console.log('user connected:', socket.id);
+
+        socket.on('joinRoom', ({ user, room }) => {
+            socket.join(room);
+            console.log(`${user} entrou na ${room}`);
         });
-    }
+
+        socket.on('message', (msg) => {
+            if (msg.room) {
+                this.io.to(msg.room).emit('message', msg);
+            } else {
+                this.io.emit('message', msg);
+            }
+            console.log("Mensagem recebida:", msg);
+        });
+    });
 }
 
-const app = new App();
+  setupRoutes() {
+    this.app.get('/', (_, res) => {
+      res.sendFile(path.join(__dirname, 'entrada.html'))
+    })
 
-app.listenServer();
+    this.app.get('/chat', (_, res) => {
+      res.sendFile(path.join(__dirname, 'index.html'))
+    })
+  }
+}
+
+const app = new App()
+app.listenServer()
